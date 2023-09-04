0b31092a2d4e31320127bc80123f0662d0d5fc9c
e-young <duddl4766@gmail.com>
2023-09-05 02:20:42 +0900

    마무리

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from "react";
-import{BrowserRouter, Route, Routes} from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomeView from "./views/HomeView";
+import lenis from "./utils/lenis";
+import link from "./utils/link";
 
-function App() {
-  return(
+const App = () => {
+    useEffect(() => {
+        lenis();
+        link();
+    }, []);
+
+    return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeView />} />
-      </Routes>
+        <Routes>
+            <Route path="/" element={<HomeView />} />
+        </Routes>
     </BrowserRouter>
-  );
+    );
 };
 
 export default App;
--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-
-
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 const portText = [
     {
         num: "01",
@@ -95,15 +95,47 @@ const portText = [
 ];
 
 const Port = () => {
+    const horizontalRef = useRef(null);
+    const sectionsRef = useRef([]);
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+    
+        const horizontal = horizontalRef.current;
+        const sections = sectionsRef.current;
+    
+        let scrollTween = gsap.to(sections, {
+            xPercent: -120 * (sections.length - 1),
+            ease: "none",
+            scrollTrigger: {
+            trigger: horizontal,
+            start: "top 56px",
+            end: () => "+=" + horizontal.offsetWidth,
+            pin: true,
+            scrub: 1,
+            invalidateOnRefresh: true,
+            anticipatePin: 1,
+            },
+        });
+    
+        return () => {
+            scrollTween.kill();
+        };
+    }, []);
+
     return (
-        <section id="port">
+        <section id="port" ref={horizontalRef}>
             <div className="port__inner">
                 <div className="port__title">
                     portfolio <em>포폴 작업물</em>
                 </div>
                 <div className="port__wrap">
                     {portText.map((port, key) => (
-                        <article className={`port__item p${key + 1}`} key={key}>
+                        <article 
+                            className={`port__item p${key + 1}`} 
+                            key={key} 
+                            ref={(el) => (sectionsRef.current[key] = el)
+                        }>
                             <span className="num">{port.num}.</span>
                             <a href={port.code} target="_blank" className="img" rel="noreferrer">
                                 <img src={port.img} alt={port.name} />
