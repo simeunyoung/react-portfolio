8a3dc02c3e9646c0d3eb1122a561bb62d44b9ca1
e-young <duddl4766@gmail.com>
2023-09-05 02:09:54 +0900

    연락처

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
+const contactText = [
+    {
+        link: "https://open.kakao.com/o/gM7YLzwf",
+        title: "KAKAO : webstupids",
+    },
+    {
+        link: "mailto:webstoryboy@naver.com",
+        title: "mail : webstoryboy@naver.com",
+    },
+];
+
 const Contact = () => {
-    return <div>Contact</div>;
+    return (
+        <div className="contact__text">
+            <div className="text">
+                {contactText.map((contact, key) => (
+                    <div key={key}>
+                        <a
+                            href={contact.link}
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            {contact.title}
+                        </a>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Contact;
