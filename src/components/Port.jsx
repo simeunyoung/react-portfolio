83d3a28afa49c19e3550dbc23aa3cab5182b71ba
e-young <duddl4766@gmail.com>
2023-09-05 02:39:22 +0900

    마무리

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,6 +1,18 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import port01 from "../assets/img/port01.jpg";
+import port02 from "../assets/img/port02.jpg";
+import port03 from "../assets/img/port03.jpg";
+import port04 from "../assets/img/port04.jpg";
+import port05 from "../assets/img/port05.jpg";
+import port06 from "../assets/img/port06.jpg";
+import port07 from "../assets/img/port07.jpg";
+import port08 from "../assets/img/port08.jpg";
+import port09 from "../assets/img/port09.jpg";
+import port10 from "../assets/img/port10.jpg";
+
 const portText = [
     {
         num: "01",
